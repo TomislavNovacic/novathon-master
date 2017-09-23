c518adf352c7bd9ad90ac15a402048eec5fa0f56
Danijel T <danijel.turic@gmail.com>
2017-09-23 16:55:35 +0200

    commit

--- app/src/main/java/com/example/tnovacic/novathon/MainActivity.java
+++ app/src/main/java/com/example/tnovacic/novathon/MainActivity.java
@@ -18,9 +18,7 @@ public class MainActivity extends AppCompatActivity {
     EditText user, password;
     Intent Activity2;
 
-    private static final String LICENSE_KEY = "QIFSS7YI-G2WAMKWX-LPZUXECB-2BLCYTRR-JDLBGALA-ZNZMCVRM-JYYUQ5VL-EQQDCSWM";
 
-    private static final int REQ_CODE_OCR = 234;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         gumb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startScanning(v);
+
             }
         });
     }
@@ -49,25 +47,5 @@ public class MainActivity extends AppCompatActivity {
         startActivity(Activity2);
     }
 
-    public void startScanning(View view) {
-        Intent fullScreenIntent = new Intent(this, FullScreenOCR.class);
-        fullScreenIntent.putExtra(IntentConstants.EXTRAS_LICENSE_KEY, LICENSE_KEY);
-        BlinkInputRecognizerSettings ocrSett = new BlinkInputRecognizerSettings();
-        RawParserSettings rawSett = new RawParserSettings();
 
-        BlinkOCREngineOptions engineOptions = new BlinkOCREngineOptions();
-
-        rawSett.setOcrEngineOptions(engineOptions);
-
-        ocrSett.addParser("Raw", rawSett);
-
-        RecognitionSettings settings = new RecognitionSettings();
-        settings.setRecognizerSettingsArray(new RecognizerSettings[]{ocrSett});
-        fullScreenIntent.putExtra(IntentConstants.EXTRAS_RECOGNITION_SETTINGS, settings);
-        try {
-            startActivityForResult(fullScreenIntent, REQ_CODE_OCR);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 }
